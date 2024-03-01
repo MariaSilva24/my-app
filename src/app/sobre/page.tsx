ccdc4dc6b0c5c8d1ca92d66a5826fe20a4d6a2b6
MARIA JULIA DA SILVA <maria.silva3579>
2024-03-01 11:42:39 -0300

    Reset CSS e link

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Sobre () {
+
+    return(
+        <>
+            <h1> Sobre </h1>
+            <p> Tela sobre meu </p>
+            <Link href={"/"}> Home </Link>
+        </>
+    )
+}
